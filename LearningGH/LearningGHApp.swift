783ce8578a5a616c62277742432761f847307473
cidvile <cidvile@gmail.com>
2022-03-31 21:48:42 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by XCodeClub on 2022-03-31.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
